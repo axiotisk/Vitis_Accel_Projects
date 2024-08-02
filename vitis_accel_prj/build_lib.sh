744e13ff71820d820b634d8e286c1db2e688a688
alex-yang-upenn <chukyang@sas.upenn.edu>
2024-08-02 17:59:43 +0100

    Re-run notebook to validate

--- vitis_accel_prj/build_lib.sh
+++ vitis_accel_prj/build_lib.sh
@@ -9,7 +9,7 @@ fi
 LDFLAGS=
 INCFLAGS="-Ifirmware/ap_types/"
 PROJECT=myproject
-LIB_STAMP=E2e6261f
+LIB_STAMP=FCAa0D71
 
 ${CC} ${CFLAGS} ${INCFLAGS} -c firmware/${PROJECT}.cpp -o ${PROJECT}.o
 ${CC} ${CFLAGS} ${INCFLAGS} -c ${PROJECT}_bridge.cpp -o ${PROJECT}_bridge.o
